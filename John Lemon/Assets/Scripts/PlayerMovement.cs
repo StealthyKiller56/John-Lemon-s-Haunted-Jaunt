5b38ad84d3c685c18b6d7b04f69f01cf193b317a
StealthyKiller56 <jjo020409@gmail.com>
2024-01-16 14:19:44 -0800

    Finished Lesson 2

--- John Lemon/Assets/Scripts/PlayerMovement.cs
+++ John Lemon/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
 
+    public float turnSpeed = 20f;
     Vector3 m_Movement;
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Quaternion m_Rotation = Quaternion.identity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,7 +29,16 @@ public class PlayerMovements : MonoBehaviour
 
         bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
-
         bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
