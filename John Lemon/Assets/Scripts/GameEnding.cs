a9a3da02160499130755a4f70f56b12a08a5c95e
StealthyKiller56 <jjo020409@gmail.com>
2024-01-18 14:23:25 -0800

    Left off Lesson 5 Step 12

---
+++ John Lemon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+
+
+    public float displayImageDuration = 1f;
+    public float fadeDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
